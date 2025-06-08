d321416b13910ef39b741e21b03aca6bffda376b
APSN4 <tema.sergeevich2012@yandex.ru>
2025-06-08 09:18:28 -0700

    upd: cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,7 +24,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://outsource.tomsk-center.ru"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
